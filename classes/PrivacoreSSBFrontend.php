f7ede74cdef81034af89112e2f7fa05056673988
Artem Osipov <artem.osipov@smartpelican.com>
2017-01-26 11:52:21 +0200

    Fix bugs with show/hide ssb buttons

--- classes/PrivacoreSSBFrontend.php
+++ classes/PrivacoreSSBFrontend.php
@@ -46,7 +46,7 @@ if (!class_exists('PrivacoreSSBFrontend')) {
             $data = array();
 
             //Make this check only if function is not called from shotcode function
-            if ($isShortcode != true) {
+            if ($isShortcode = true) {
                 $displayOnPage = get_post_meta($post->ID, 'pssb_show_on_page', true);
 
                 if ($displayOnPage == 0) {
@@ -75,7 +75,7 @@ if (!class_exists('PrivacoreSSBFrontend')) {
             $data['facebook_title'] = get_post_meta($post->ID, 'pssb_facebook_title', true) != '' ? get_post_meta($post->ID, 'pssb_facebook_title', true) : mb_substr(get_the_title($post->ID), 0, 100);
             $data['facebook_description'] = get_post_meta($post->ID, 'pssb_facebook_description', true) != '' ? get_post_meta($post->ID, 'pssb_facebook_description', true) : mb_substr($excerpt, 0, 301);
 
-            $data['hide_on_page'] = get_post_meta($post->ID, 'pssb_show_on_page', true);
+//           $data['hide_on_page'] = get_post_meta($post->ID, 'pssb_show_on_page', true);
 
             return $data;
         }
@@ -88,7 +88,7 @@ if (!class_exists('PrivacoreSSBFrontend')) {
         private function _getRenderedView()
         {
             $data = $this->_pssbButtonsData();
-			if($data['hide_on_page']==1){
+			if(!$data['hide_on_page']){
                 return $this->_loadView('front-buttons', $data);
             }
         }
@@ -100,7 +100,7 @@ if (!class_exists('PrivacoreSSBFrontend')) {
          */
         public function showButtonsPublic($content)
         {
-            if (is_single()) {
+            if (is_single() || is_page()) {
                 $content .= $this->_getRenderedView();
             }
             return $content;
@@ -113,13 +113,11 @@ if (!class_exists('PrivacoreSSBFrontend')) {
         public function pssbShortcode()
         {
             $data = $this->_pssbButtonsData(true);
-			if($data['hide_on_page']==1){
+			if(!$data['hide_on_page']){
 				//use different view file for shortcode
 				return $this->_loadView('shortcodes/buttons-sc', $data);
 			}
         }
-
-
     }
 
 }
